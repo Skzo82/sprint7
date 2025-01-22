ab9118b5d04b8eada950b815176c2086065bf170
Skzo <francesco@friargiu.ru>
2025-01-22 23:33:09 +0300

    sprint7.0

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -6,29 +6,30 @@ import tasks.Task;
 import tasks.Epic;
 import tasks.Subtask;
 
-import org.junit.jupiter.api.Disabled;
 import java.io.File;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FileBackedTaskManagerTest {
-    private final File file = new File("test.csv"); // временный файл для тестирования
+    private final File file = new File("test.csv");
     private FileBackedTaskManager taskManager = new FileBackedTaskManager(file);
 
     @Test
     public void testSaveAndLoad() {
+
         Task task = new Task("Test Task", "This is a test task.");
         taskManager.addNewTask(task);
 
         Epic epic = new Epic("Test Epic", "Epic description");
-        taskManager.addNewEpic(epic);
+        int epicId = taskManager.addNewEpic(epic); // Otteniamo l'ID dell'epic
 
-        Subtask subtask = new Subtask("Test Subtask", "Subtask description", epic.getId());
+        Subtask subtask = new Subtask("Test Subtask", "Subtask description", epicId);
         taskManager.addNewSubtask(subtask);
 
         taskManager = FileBackedTaskManager.loadFromFile(file);
 
+
         assertNotNull(taskManager.getTask(task.getId()), "Task should be loaded.");
-        assertNotNull(taskManager.getEpic(epic.getId()), "Epic should be loaded.");
+        assertNotNull(taskManager.getEpic(epicId), "Epic should be loaded."); // Usa epicId
         assertNotNull(taskManager.getSubtask(subtask.getId()), "Subtask should be loaded.");
     }
 }
