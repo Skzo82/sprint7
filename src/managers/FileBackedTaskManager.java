52b91162fcf1b767289009e1d4d8400f5c201652
Skzo <francesco@friargiu.ru>
2025-04-22 20:37:07 +0300

    final ver.7

--- src/managers/FileBackedTaskManager.java
+++ src/managers/FileBackedTaskManager.java
@@ -73,7 +73,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
 
     private void save() {
         StringBuilder sb = new StringBuilder();
-        sb.append("id,type,name,status,description,epic");
+        sb.append("id,type,name,status,description,epic\n");
 
         for (Task task : getTasks()) {
             sb.append(toString(task)).append("\n");
