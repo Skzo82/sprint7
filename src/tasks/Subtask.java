7dce8f1ef619e8cf49ca100ff5d1633f7ab587c0
Skzo <francesco@friargiu.ru>
2025-05-22 19:23:05 +0300

    нов.1.9

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -44,6 +44,7 @@ public class Subtask extends Task {
                 ", epicId=" + epicId +
                 '}';
     }
+
     @Override
     public TaskType getType() {
         return TaskType.SUBTASK;
